955f92ed01726c13e56c59fbccd4ad71c432f76e
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-11 19:52:02 +0100

    artist class added

---
+++ modules/artist.rb
@@ -0,0 +1,11 @@
+require('pg')
+require_relative('album.rb')
+
+class Artist
+
+  attr_reader(:id, :name)
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
